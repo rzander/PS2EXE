67af771bef6ba97b29985934eda334c9e6218464
Roger Zander <roger@zander.ch>
2016-11-11 18:20:10 +0100

    Initial upload

---
+++ PS2EXE/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PS2EXE.Default1" validateRequest="false" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="col-md-10 col-md-offset-1 container">
+<%--        <div class="jumbotron">
+            <p>test</p>
+            <sup>10.Nov.2016</sup>
+        </div>--%>
+        <div class="row">
+
+            <div class="col-md-12 form-group">
+                <label>PowerShell Code:</label>
+                <asp:TextBox ID="tb_PSSCript" runat="server" class="form-control" required="required" placeholder="Paste your PowerShell script here..." Rows="20" TextMode="MultiLine" BackColor="#012456" ForeColor="White" Font-Names="Helvetica" Font-Bold="False"></asp:TextBox>
+            </div>
+            <div class="col-md-12 form-group">
+                <label>Filename:</label>
+                <asp:TextBox ID="tb_Filename" runat="server" class="form-control" required="required" placeholder="FileName.exe" TextMode="SingleLine"></asp:TextBox>
+            </div>
+        </div>
+        <br />
+        <div>
+            <asp:LinkButton ID="bt_Compile" runat="server" CssClass="btn btn-primary" OnClick="bt_Compile_Click">
+                <span aria-hidden="true" class="glyphicon glyphicon-send"></span> Create EXE
+            </asp:LinkButton>
+        </div>
+    </div>
+</asp:Content>
---
+++ PS2EXE/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace PS2EXE
+{
+    public partial class Default1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bt_Compile_Click(object sender, EventArgs e)
+        {
+            string sFilename = Environment.ExpandEnvironmentVariables(Server.MapPath("~/App_Data/" + Path.GetRandomFileName().Split('.')[0] + ".exe"));
+            //string sResname = Environment.ExpandEnvironmentVariables(Server.MapPath("~/App_Data/" + Path.GetRandomFileName().Split('.')[0]));
+            //Directory.CreateDirectory(sResname);
+
+
+            //string sFilename = Environment.ExpandEnvironmentVariables(Server.MapPath("~/App_Data/" + tb_Filename.Text));
+            string sResname = Environment.ExpandEnvironmentVariables(Server.MapPath("~/App_Data/Resources.resx"));
+            try
+            {
+                File.Delete(sFilename);
+                File.Delete(sResname);
+            }
+            catch { }
+
+            CreateExe oExe = new CreateExe(sFilename, Server.MapPath("~/Bin"));
+            oExe.cp.CompilerOptions = Environment.ExpandEnvironmentVariables("/win32icon:\"" + Server.MapPath("~/Images/powershell.ico") +"\" /optimize");
+
+            /*if (imgIcon.Tag != null)
+                oExe.Icon = imgIcon.Tag as byte[];*/
+
+            //oExe.Sources.Add(tbPSCode.Text);
+            oExe.Sources.Add(Properties.Resources.Source);
+            oExe.Sources.Add(Properties.Resources.Assembly);
+
+            System.Resources.ResourceWriter writer = new System.Resources.ResourceWriter(sResname);
+            writer.AddResource("psCode.ps1", " " + tb_PSSCript.Text.Trim());
+            writer.Generate();
+            writer.Close();
+            oExe.cp.EmbeddedResources.Add(sResname);
+
+            if (!oExe.Compile())
+            {
+                oExe.ToString();
+                //MessageBox.Show("Failed to create .Exe", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            //File.Delete(sResname);
+            //Directory.Delete(sResname);
+
+            Response.ContentType = "application/octet-stream";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + tb_Filename.Text);
+            Response.TransmitFile(sFilename);
+            Response.End();
+
+            /*
+            try
+            {
+                File.Delete(sFilename);
+                File.Delete(sResname);
+            }
+            catch { } */
+        }
+    }
+}
